def08339c7041690ed9121c5c67ea562d2ffd2b5
impactCn <550569627@qq.com>
2021-01-27 01:20:26 +0800

    feat: time 1.0v

--- src/com/tool/utils/TimeUtils.java
+++ src/com/tool/utils/TimeUtils.java
@@ -1,11 +1,9 @@
 package com.tool.utils;
 
-import com.tool.annotion.NotNull;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.Formatter;
 
 /**
  * @author: impactCn
@@ -13,33 +11,97 @@ import java.util.Formatter;
  */
 public class TimeUtils {
 
+    private static ZoneId zoneId = ZoneId.systemDefault();
 
+    private static DateTimeFormatter formatter;
 
-    public static Date localDateToDate(@NotNull LocalDate localDate) {
+    public static Date localDateToDate(LocalDate localDate) {
 
-
-        return new Date();
+        return Date.from(localDate.atStartOfDay(zoneId).toInstant());
     }
+
+    public static Date minus(LocalDate localDate, long year, long month, long day) {
+        return Date.from(localDate
+                .minusYears(year)
+                .minusMonths(month)
+                .minusDays(day)
+                .atStartOfDay(zoneId)
+                .toInstant());
+    }
+
+    public static Date minus(LocalDate localDate, long year, long month) {
+        return Date.from(localDate
+                .minusYears(year)
+                .minusMonths(month)
+                .atStartOfDay(zoneId)
+                .toInstant());
+    }
+
+    public static Date minus(LocalDate localDate, long year) {
+        return Date.from(localDate
+                .minusYears(year)
+                .atStartOfDay(zoneId)
+                .toInstant());
+    }
+
 
     public static Date localDateTimeToDate(LocalDateTime localDateTime) {
 
-        return new Date();
+        return Date.from(localDateTime.atZone(zoneId).toInstant());
     }
 
-    public static String dateFormat(Date date, String format) {
-        return null;
+
+    public static LocalDate dateToLocalDate(Date date) {
+
+        return date.toInstant().atZone(zoneId).toLocalDate();
+
     }
 
-    public static String zeroToday() {
-        return null;
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+
+        return date.toInstant().atZone(zoneId).toLocalDateTime();
     }
 
-    public static String twelveToday() {
-        return null;
+    public static String format(Date date, String format) {
+        formatter = DateTimeFormatter.ofPattern(format);
+
+        return dateToLocalDate(date).format(formatter);
+
     }
 
-    public static int dateRange(String firstDate, String secondDate) {
-        return 0;
+    public static String format(LocalDate localDate, String format) {
+        formatter = DateTimeFormatter.ofPattern(format);
+
+        return localDate.format(formatter);
+    }
+
+    public static String format(LocalDateTime localDateTime, String format) {
+        formatter = DateTimeFormatter.ofPattern(format);
+
+        return localDateTime.format(formatter);
+    }
+
+    public static LocalDateTime minOfToday() {
+
+        return LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+    }
+
+
+    public static LocalDateTime maxOfToday() {
+        return LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
+    }
+
+    public static LocalDateTime noonOfToday() {
+        return LocalDateTime.of(LocalDate.now(), LocalTime.NOON);
+    }
+
+    public static LocalDateTime midnightOfToday() {
+        return LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT);
+    }
+
+    public static long dateRange(Date startTime, Date endTime) {
+
+        return  dateToLocalDate(endTime).toEpochDay() - dateToLocalDate(startTime).toEpochDay();
     }
 
 
